bc94de4b88b34ab1b1712f5a7f013dce5468de72
panuchart <panuchart.bun@gmail.com>
2017-06-26 06:53:22 +0000

    Fixed error

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hello World");
-	//insert some error here
-	System.out.println("Hello Again);
+	System.out.println("Hello Again");
     }
 }
